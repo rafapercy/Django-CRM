f971dd427b8d4e467a08ea8647f0effaef4f06f3
Rafa Percy <rafa.percy28@gmail.com>
2024-12-13 23:57:53 +0800

    Final Project

--- website/models.py
+++ website/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+
+class Record(models.Model):
+	created_at = models.DateTimeField(auto_now_add=True)
+	album_title = models.CharField(max_length=500)
+	artist_name = models.CharField(max_length=500)
+	release_year = models.CharField(max_length=500)
+	genre = models.CharField(max_length=500)
+	inclusions = models.CharField(max_length=1000)
+
+	def __str__(self): 
+		return(f"{self.album_title} {self.artist_name}")
